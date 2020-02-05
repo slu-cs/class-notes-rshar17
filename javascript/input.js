199d4ac6e9d9ed443381e756da4634e662314de4
Rajat Sharma <rshar17@stlawu.edu>
2020-02-05 13:59:33 -0500

    javascript file input

--- javascript/input.js
+++ javascript/input.js
@@ -1,7 +1,7 @@
 //sytax for console and file input
 
 const readline = require('readline');
-
+const fs = require('fs');
 //Console configuration
 const user = readline.createInterface({
 
@@ -12,7 +12,21 @@ const user = readline.createInterface({
 //console input
 user.question('Filename: ', function(filename)
 {
-  console.log(filename);
+  //File configuration
+  const file = readline.createInterface({
+    input: fs.createReadStream(filename);
+  });
+  // Asynchronous line by line input
+  file.on('line',function(line)
+  {
+    console.log(line);
+  });
+
+  // end program
+  file.on('close',function()
+  {
+    process.exit(0);
+  });
 
 });
 
